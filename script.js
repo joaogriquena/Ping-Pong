e60e1da4ae2281921aeeeec8cb2ac9c37e7350e6
Joao Guilherme <jgriquena@gmail.com>
2023-05-14 11:01:20 -0300

    linha central

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvasEl = document.querySelector('canvas'),
     canvasCtx = canvasEl.getContext('2d')
 
+const lineWidth = 15
+
 function setup() {
     canvasEl.width = window.innerWidth
     canvasEl.height = window.innerHeight
@@ -12,9 +14,16 @@ function setup() {
 function draw() {
     canvasCtx.fillStyle = "#286047"
     canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+
+    canvasCtx.fillStyle = "#fff"
+
+    const x = window.innerWidth / 2 - lineWidth / 2
+    const y = 0
+    const w = lineWidth
+    const h = window.innerHeight
+
+    canvasCtx.fillRect(x, y, w, h)
 }
-
-
 
 setup()
 draw()
